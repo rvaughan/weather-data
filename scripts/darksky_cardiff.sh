de4c34bb840b8523316c9d12c188cc06999e2c25
Richard Vaughan <richard@purplemonkeys.co.uk>
2020-07-18 09:25:10 +0100

    Make binaries explicit in scripts to aid cron job usage.

--- scripts/darksky_cardiff.sh
+++ scripts/darksky_cardiff.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-git pull
+/usr/bin/git pull
 
 cd ..
 mkdir -p cardiff/darksky && cd cardiff/darksky
-wget -q -O weather_new.json "https://api.darksky.net/forecast/256d6f395303ef80d4414e5a116279f1/51.4816,3.1791"
-jq . weather_new.json > weather.json
+/usr/bin/wget -q -O weather_new.json "https://api.darksky.net/forecast/256d6f395303ef80d4414e5a116279f1/51.4816,3.1791"
+/usr/bin/jq . weather_new.json > weather.json
 rm weather_new.json
 
-git commit -m 'Latest DarkSky Weather data for Cardiff' weather.json
-git push
+/usr/bin/git commit -m 'Latest DarkSky Weather data for Cardiff' weather.json
+/usr/bin/git push
